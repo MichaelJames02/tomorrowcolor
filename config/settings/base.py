d814c711dfee68cce2d13329a7ad9f2294edf510
michaeljames02 <michael4achieve@gmail.com>
2018-04-22 23:43:26 -0300

    remove django-heroku

--- config/settings/base.py
+++ config/settings/base.py
@@ -271,6 +271,4 @@ SOCIALACCOUNT_ADAPTER = 'tomorrowcolor.users.adapters.SocialAccountAdapter'
 
 # Your stuff...
 # ------------------------------------------------------------------------------
-# Activate Django-Heroku.
-import django_heroku
-django_heroku.settings(locals())
+
